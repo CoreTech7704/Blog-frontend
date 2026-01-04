201bf8a857020b694c2e467ce5eacab3c3cb5ad9
Sarvam Patel <148036885+CoreTech7704@users.noreply.github.com>
2026-01-04 13:02:00 +0530

    home page hero section update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,47 +1,67 @@
-import Orb from "@/components/ui/Orb";
+import FloatingLines from "@/components/ui/FloatingLines";
+import { ArrowRight } from 'lucide-react';
 
 export default function Hero() {
   return (
     <section className="relative min-h-screen overflow-hidden bg-[#05070d]">
-
-      {/* ORB BACKGROUND */}
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
-        <div className="w-[900px] h-[900px]">
-          <Orb
-            hoverIntensity={0}
-            rotateOnHover={false}
-            forceHoverState={false}
+      {/* FloatingLines BACKGROUND */}
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center opacity-70">
+        <div className="w-[100%] h-[100vh]">
+          <FloatingLines
+            enabledWaves="top,middle,bottom"
+            lineDistance={15}
+            bendStrength={0.5}
           />
         </div>
       </div>
 
       {/* HERO CONTENT */}
       <div className="relative z-10 flex min-h-screen flex-col items-center justify-center px-6 text-center">
-        <h1 className="mb-6 text-5xl font-extrabold leading-tight text-white sm:text-6xl md:text-7xl">
+        <h1 className="mb-2 text-5xl font-extrabold leading-tight text-white sm:text-6xl md:text-7xl">
           Welcome to{" "}
-          <span className="bg-gradient-to-r from-cyan-400 to-violet-400 bg-clip-text text-transparent">
+          <span className="bg-gradient-to-r from-cyan-400 to-violet-400 bg-clip-text text-transparent text-shadow-xl ">
             VoidWork
           </span>
         </h1>
 
-        <p className="mb-8 max-w-2xl text-lg text-slate-300">
+        <p className="mb-6 max-w-2xl text-lg text-slate-300">
           Dive into the world of coding with our expertly crafted blogs,
           tutorials, and resources designed to empower developers of all levels.
         </p>
 
-        <a
-          href="#get-started"
-          className="
-            inline-block rounded-full
-            bg-gradient-to-r from-cyan-500 to-violet-500
-            px-8 py-4 text-lg font-semibold text-stone-950
+        <div className="flex gap-4">
+          <a
+          href="#Explore-Blogs"
+          className=" relative flex items-center gap-2
+            rounded-xl px-6 py-3 text-md font-semibold text-white
+            bg-gradient-to-r from-cyan-500 to-violet-600
+            backdrop-blur-md 
+            border border-white
+            shadow-lg shadow-violet-500/30
             transition-all duration-300
-            hover:scale-105 hover:shadow-lg hover:shadow-cyan-500/25
+            hover:scale-105 hover:shadow-xl hover:shadow-violet-500/40
             active:scale-95
           "
         >
-          Get Started
+          Explore Blogs 
+          <ArrowRight className="transition-transform duration-300 group-hover:translate-x-1" />
         </a>
+        <a
+          href="#get-started"
+          className="relative inline-flex items-center justify-center
+            px-6 py-2 rounded-xl
+            text-md font-semibold text-white/80
+            bg-white/5 backdrop-blur-md
+            border border-white
+            transition-all duration-300
+            hover:bg-white/10 hover:text-white
+            hover:scale-105 hover:shadow-lg hover:shadow-white/20
+            active:scale-95            
+          "
+        >
+          Join Voidwork
+        </a>
+        </div>
       </div>
 
       {/* BOTTOM FADE */}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,23 @@ export default function Home() {
       <p className="text-slate-600 dark:text-slate-300 mt-2">
         Welcome to Core Blog
       </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusamus perspiciatis quam repudiandae, quas nobis fugiat totam praesentium maiores, cum commodi tempore. Accusantium voluptatum rerum doloribus temporibus, suscipit amet animi.
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusamus perspiciatis quam repudiandae, quas nobis fugiat totam praesentium maiores, cum commodi tempore. Accusantium voluptatum rerum doloribus temporibus, suscipit amet animi.
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusamus perspiciatis quam repudiandae, quas nobis fugiat totam praesentium maiores, cum commodi tempore. Accusantium voluptatum rerum doloribus temporibus, suscipit amet animi.
+      </p>
+
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusamus perspiciatis quam repudiandae, quas nobis fugiat totam praesentium maiores, cum commodi tempore. Accusantium voluptatum rerum doloribus temporibus, suscipit amet animi.
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusamus perspiciatis quam repudiandae, quas nobis fugiat totam praesentium maiores, cum commodi tempore. Accusantium voluptatum rerum doloribus temporibus, suscipit amet animi.
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusamus perspiciatis quam repudiandae, quas nobis fugiat totam praesentium maiores, cum commodi tempore. Accusantium voluptatum rerum doloribus temporibus, suscipit amet animi.
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusamus perspiciatis quam repudiandae, quas nobis fugiat totam praesentium maiores, cum commodi tempore. Accusantium voluptatum rerum doloribus temporibus, suscipit amet animi.
+      </p>
     </main>
+  
   )
 }
