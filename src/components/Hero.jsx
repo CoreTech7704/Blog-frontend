b11589c715f00044043179a351c8a0d509e8ebe2
CoreTech7704 <sarvampatel456@gmail.com>
2026-02-02 18:09:01 +0530

    Update: small UI polish on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
 import FloatingLines from "@/components/ui/FloatingLines";
 import { ArrowRight } from 'lucide-react';
 import { Link } from "react-router-dom";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function Hero() {
+
+  const { user } = useAuth();
+
   return (
     <section className="relative min-h-screen overflow-hidden bg-[#05070d]">
       {/* FloatingLines BACKGROUND */}
@@ -32,8 +36,8 @@ export default function Hero() {
 
         <div className="flex gap-4">
           <Link
-          to="/blogs"
-          className=" relative flex items-center gap-2
+            to="/blogs"
+            className=" relative flex items-center gap-2
             rounded-xl px-6 py-3 text-md font-semibold text-white
             bg-gradient-to-r from-cyan-500 to-violet-600
             backdrop-blur-md 
@@ -43,25 +47,29 @@ export default function Hero() {
             hover:scale-105 hover:shadow-xl hover:shadow-violet-500/40
             active:scale-95
           "
-        >
-          Explore Blogs 
-          <ArrowRight className="transition-transform duration-300 group-hover:translate-x-1" />
-        </Link>
-        <Link
-          to="/auth"
-          className="relative inline-flex items-center justify-center
-            px-6 py-2 rounded-xl
-            text-md font-semibold text-white/80
-            bg-white/5 backdrop-blur-md
-            border border-white
-            transition-all duration-300
-            hover:bg-white/10 hover:text-white
-            hover:scale-105 hover:shadow-lg hover:shadow-white/20
-            active:scale-95            
-          "
-        >
-          Join Voidwork
-        </Link>
+          >
+            Explore Blogs
+            <ArrowRight className="transition-transform duration-300 group-hover:translate-x-1" />
+          </Link>
+
+          {!user && (
+            <Link
+              to="/auth"
+              className="relative inline-flex items-center justify-center
+                          px-6 py-2 rounded-xl
+                          text-md font-semibold text-white/80
+                          bg-white/5 backdrop-blur-md
+                          border border-white
+                          transition-all duration-300
+                          hover:bg-white/10 hover:text-white
+                          hover:scale-105 hover:shadow-lg hover:shadow-white/20
+                          active:scale-95            
+                        "
+            >
+              Join VoidWork
+            </Link>
+          )}
+
         </div>
       </div>
 
